ee328ffd7b2796b81ce47ece81a8e7f7809d4ce6
DavidLim93 <lim.david93@gmail.com>
2022-08-06 07:21:30 +1000

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const name = [ 'Lisa', 'Kaitlin', 'Jan' ];
+
+function writeCards(names, event) {
+    let cards = []
+    for (let i = 0; i < names.length; i++) {
+      cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards 
+  }
+
+const number = 10
+  function countDown(number) {
+    while (number>-1) {
+        console.log(number--)
+    }
+        return number;
+  }
